f725d0a1b8130f9facb35675642030ea93ec31a9
CW-Logothetis <cwlogo@pm.me>
2023-01-29 16:22:11 +0200

    add hardcoded list of array methods

---
+++ src/lib/arrayMethods.js
@@ -0,0 +1,150 @@
+export let arrayMethods = [
+    {
+        method: "at()",
+        description: "Return element at requested index "
+    },
+    {
+        method: "concat()",
+        description: "Join arrays. Then return new array"
+    },
+    {
+        method: "copyWithin()",
+        description: "Copy element to other index in same array"
+    },
+    {
+        method: "entries()",
+        description: "Return key:value pair"
+    },
+    {
+        method: "every()",
+        description: "Test if EVERY element passes a given test"
+    },
+    {
+        method: "fill()",
+        description: "Fill array with a static value"
+    },
+    {
+        method: "filter()",
+        description: "Create NEW array with elements in orig. array that pass a test"
+    },
+    {
+        method: "find()",
+        description: "Return value of the 1st element that passes a test (L-R)"
+    },
+    {
+        method: "findIndex()",
+        description: "Return index of 1st element that passes a test (L-R)"
+    },
+    {
+        method: "findLast()",
+        description: "Return value of 1st element that passes a test (R-L)"
+    },
+    {
+        method: "findLastIndex()",
+        description: "Return index of 1st element that passes a test (R-L)"
+    },
+    {
+        method: "flat()",
+        description: "Return NEW array to concatanate nested arrays or remove empty elements"
+    },
+    {
+        method: "flatMap()",
+        description: "Return NEW array with map() then flat()"
+    },
+    {
+        method: "forEach()",
+        description: "Run a function on each element"
+    },
+    {
+        method: "from()",
+        description: "Create an array from an object"
+    },
+    {
+        method: "includes()",
+        description: "Check if array includes your requested element"
+    },
+    {
+        method: "indexOf()",
+        description: "Search for an element. Returns its position"
+    },
+    {
+        method: "isArray()",
+        description: "Check if object is an array"
+    },
+    {
+        method: "join()",
+        description: "Join all elements into one string"
+    },
+    {
+        method: "keys()",
+        description: "Returns an object with an array's keys"
+    },
+    {
+        method: "lastIndexOf()",
+        description: "Search for an element, from array's end. Return el position"
+    },
+    {
+        method: "map()",
+        description: "Create NEW array from results of running a function on each element"
+    },
+    {
+        method: "of()",
+        description: "Create array from arguments passed to it"
+    },
+    {
+        method: "pop()",
+        description: "Remove last element. Return element"
+    },
+    {
+        method: "push()",
+        description: "Add new elements to end. Return new length"
+    },
+    {
+        method: "reduce()",
+        description: "Convert all elements into a single value (L-to-R)"
+    },
+    {
+        method: "reduceRight()",
+        description: "Convert all elements into a single value (R-to-L)"
+    },
+    {
+        method: "reverse()",
+        description: "Reverse order of elements"
+    },
+    {
+        method: "shift()",
+        description: "Remove 1st element. Return element."
+    },
+    {
+        method: "slice()",
+        description: "Select section of array. Return as NEW array"
+    },
+    {
+        method: "some()",
+        description: "Test if ANY elements pass a given test"
+    },
+    {
+        method: "sort()",
+        description: "Sort elements"
+    },
+    {
+        method: "splice()",
+        description: "Add or remove elements"
+    },
+    {
+        method: "toLocaleString()",
+        description: "Convert array to string with OPTIONS. Return the string"
+    },
+    {
+        method: "toString()",
+        description: "Convert array to string. Return the string"
+    },
+    {
+        method: "unshift()",
+        description: "Adds new elements to start. Return new length"
+    },
+    {
+        method: "values()",
+        description: "Return NEW array iterator object that iterates value of each index."
+    }
+]
