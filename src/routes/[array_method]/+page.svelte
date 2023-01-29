e7168793b2e8458d3557fbb5660c859faf304582
CW-Logothetis <cwlogo@pm.me>
2023-01-29 18:24:42 +0200

    add dynamic array method page

---
+++ src/routes/[array_method]/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+// @ts-nocheck
+    export let data
+    const { output } = data
+    console.log(output)
+</script>
+
+
+<section class='returned-text'>
+    {#if !output}
+        Loading...
+    {:else}
+        {output}
+    {/if}
+</section>
+
+<style>
+    .returned-text {
+        background-color: rgba(58, 58, 99, 0.5);
+        border-radius: 10px;
+        color: aliceblue;
+        margin: 3rem auto;
+        padding: 0 2rem 2rem 2rem;
+        width: 50%;
+        white-space: pre-wrap; 
+    }
+</style>
